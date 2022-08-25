9287fb1857568cf6c76788e17b8d535e0abc18fb
robos <robosam2003@gmail.com>
2022-08-26 00:12:13 +0100

    implement euler calcs from accel

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #define CS_PIN 10
 LSM6DSO32 LSM(CS_PIN, SPI, 400000);
 
+#define RAD_TO_DEG 180/PI
+
 void setup() {
 // write your initialization code here
     Serial.begin(115200);
@@ -19,6 +21,9 @@ void loop() {
     while(LSM.get_accel_drdy_status() != 1);
     Vector<double, 3> acc = LSM.get_accel();
 
+    Vector<double, 3> euler = {0,0,0};
+    euler[0] = atan2(acc[1], acc[2]) * RAD_TO_DEG;
+    euler[1] = atan2(acc[2], -acc[0]) * RAD_TO_DEG;
 
-    Serial.printf("Acc: %lf, %lf, %lf\n", acc[0], acc[1], acc[2]);
+    Serial.printf("Euler: %f %f %f\n", euler[0], euler[1], euler[2]);
 }
