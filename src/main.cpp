579228b88fb25bd0ff7dcc5ae65e923a969ccecc
robos <robosam2003@gmail.com>
2022-08-25 23:48:30 +0100

    sorted library structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <Arduino.h>
+
+#include "LSM6DSO32.h"
+
+#define CS_PIN 10
+LSM6DSO32 LSM(CS_PIN, SPI, 400000);
+
 void setup() {
 // write your initialization code here
+    Serial.begin(115200);
+
+    LSM.begin();
+
+    LSM.default_configuration();
+
 }
 
 void loop() {
-// write your code here
+    while(LSM.get_accel_drdy_status() != 1);
+    Vector<double, 3> acc = LSM.get_accel();
+
+
+    Serial.printf("Acc: %lf, %lf, %lf\n", acc[0], acc[1], acc[2]);
 }
