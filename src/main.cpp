2d6883b5e68466a9ccc932ed58315551ab45b65e
robos <robosam2003@gmail.com>
2022-12-20 18:15:46 +0000

    add eigen, change to quaternion rotation system over euler

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
-#include <Arduino.h>
+#include "Arduino.h"
+
+// Eigen
+#include "eigen.h"
+#include "Eigen/Core"
+#include "Eigen/Geometry"
 
 #include "LSM6DSO32.h"
 #include "RCFilter.h"
@@ -9,6 +14,7 @@ LSM6DSO32 LSM(CS_PIN, SPI, 400000);
 
 #define RAD_TO_DEG 180/PI
 
+
 void setup() {
 // write your initialization code here
     Serial.begin(115200);
@@ -16,43 +22,57 @@ void setup() {
     LSM.begin();
 
     LSM.default_configuration();
-    LSM.set_accel_ODR(OUTPUT_DATA_RATES::ODR_104_HZ);
-    LSM.set_gyro_ODR(OUTPUT_DATA_RATES::ODR_104_HZ);
+    LSM.set_accel_ODR(OUTPUT_DATA_RATES::ODR_1667_HZ);
+    LSM.set_gyro_ODR(OUTPUT_DATA_RATES::ODR_1667_HZ);
     LSM.set_accel_high_pass_or_LPF2_filter_cutoff(ACCEL_HP_OR_LPF2_CUTOFF::ODR_OVER_100);
 
 }
 
 
-// everything in radians for calc.
-Vector<double, 3> eul_hat = {0,0,0};
+Eigen::Quaterniond q(1, 0, 0, 0);
 double alpha = 0.01;
 unsigned long prevTime_us = 0;
 void loop() {
+
+
     // Accelerometer estimation - assume body is not accelerating.
     while(LSM.get_accel_drdy_status() == 0);
     Vector<double, 3> acc = LSM.get_accel();
-    Vector<double, 3> gyr = LSM.get_gyro();
-    gyr *= DEG_TO_RAD;
-    Vector<double, 3> euler_hat_a = {0,0,0};
+    Vector<double, 3> gyr = LSM.get_gyro(); gyr*=DEG_TO_RAD;
 
+    // Find orientation according to the accelerometer
+    Eigen::Quaterniond q_body_a(q.w(), acc[0], acc[1], acc[2]);
+    q_body_a.normalize();
 
-    euler_hat_a[0] = atan2(acc[1], acc[2]);
-    euler_hat_a[1] = atan2(-acc[0], sqrt(pow(acc[1], 2) + pow(acc[2], 2)));
-
-    // Gyroscope
-    //while(LSM.get_gyr_drdy_status() == 0);
-
-    Vector<double, 3> eul_rate_g = {0,0,0};
-
-    eul_rate_g[0] = gyr[0] + gyr[1]*sin(eul_hat[0])*tan(eul_hat[1]) + gyr[2]*cos(eul_hat[0])*tan(eul_hat[1]);
-    eul_rate_g[1] = 0      + gyr[1]*cos(eul_hat[0])                 + gyr[2]*(-sin(eul_hat[0]));
-
-
-    // Complementary filter
+    // Find orientation according to the gyroscope
     unsigned long currentTime_us = micros();
-    eul_hat[0] = euler_hat_a[0]*alpha  +  (1-alpha)*(eul_hat[0] + (1e-6)*(currentTime_us-prevTime_us)*eul_rate_g[0]);
-    eul_hat[1] = euler_hat_a[1]*alpha  +  (1-alpha)*(eul_hat[1] + (1e-6)*(currentTime_us-prevTime_us)*eul_rate_g[1]);
+    double theta = gyr.norm() * (currentTime_us - prevTime_us)*(1e-6);
     prevTime_us = currentTime_us;
 
-    Serial.printf("EUL_HAT: %lf, %lf, %lf\n", eul_hat[0]*RAD_TO_DEG, eul_hat[1]*RAD_TO_DEG, eul_hat[2]*RAD_TO_DEG);
+    Vector<double, 3> v = gyr.normalized();
+
+    Eigen::Quaterniond q_delta_ori_g( cos(theta/2), v[0]*sin(theta/2), v[1]*sin(theta/2), v[2]*sin(theta/2) );
+    Eigen::Quaterniond q_body_g = q*q_delta_ori_g;
+    q_delta_ori_g.normalize();
+    if (std::isnan(q_delta_ori_g.w())) {
+        q_delta_ori_g.w() = 1;
+        q_delta_ori_g.x() = 0;
+        q_delta_ori_g.y() = 0;
+        q_delta_ori_g.z() = 0;
+    }
+
+
+    // Find orientation according to the accelerometer and gyroscope
+    Eigen::Quaterniond q_body = q_body_g.slerp(alpha, q_body_a);
+    q_body.normalize();
+    q = q_body;
+
+
+
+    // /r/n = CR/LF for MATLAB
+    Serial.printf("%lf, %lf, %lf, %lf\r\n", q.w(), q.x(), q.y(), q.z());
+    delay(5);
+
+    // Quaternions are beautiful.
 }
+
